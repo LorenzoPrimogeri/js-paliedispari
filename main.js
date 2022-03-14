a848e68f50f46ed94680ff0e71cdc31ee337ab3a
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-14 15:55:47 +0100

    pari o dispari complete

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function Palindroma(parola) {
+/* function Palindroma(parola) {
     let controllo = [];
     let i = 0;
     let j = parola.length;
@@ -24,12 +24,36 @@ function Palindroma(parola) {
         return "Non palindroma";
     }
 }
-
 const parola = prompt("Inserisci una parola");
 const verifica = Palindroma(parola);
-
 if (verifica == "palindroma") {
     console.log("palindroma");
 } else {
     console.log("non è palindroma");
 }
+*/
+function random(min, max) {
+    const random = Math.floor(Math.random() * max) + min;
+    return random;
+}
+function PariODispari(numeroUtente, numeroComputer) {
+    console.log("tu:" + numeroUtente, "computer:" + numeroComputer);
+    let somma = numeroUtente + numeroComputer;
+    if (somma % 2 == 0) {
+        return "pari";
+    } else {
+        return "dispari";
+    }
+
+};
+const scelta = prompt("Inserisci 'pari' o 'dispari'");
+const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+const numeroComputer = random(1, 5);
+const verifica = PariODispari(numeroUtente, numeroComputer);
+
+if (verifica == scelta) {
+    console.log("hai vinto");
+} else {
+    console.log("hai perso");
+}
+
